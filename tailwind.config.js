97419e849d6dd0fcebac03dedd7543e9c9e3420e
sanoop2hats <sanoop@2hatslogic.com>
2024-11-28 14:16:19 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './template/customers/*.liquid',
+    './template/*.liquid'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
